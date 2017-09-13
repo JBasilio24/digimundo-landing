41483ad5f61ea4f8d1c45efd843823132a8173e1
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-09-13 11:06:31 -0500

    Old changes

--- contact.php
+++ contact.php
@@ -2,18 +2,19 @@
 if(isset($_POST['submit'])){
     $to = "itzli2000@msn.com";
     $from = $_POST['email'];
-    $name = $_POST['first_name'];
+    $name = $_POST['name'];
     $subject = "Contacto desde Digimundo.";
     $subject2 = "Copia del mensaje enviado a Digimundo.";
-    $message = $name . " ". " Escribio lo siguiente:" . "\n\n" . $_POST['message'];
-    $message2 = "Esta es una copia de tu mensaje " . $first_name . "\n\n" . $_POST['message'];
+    $message = "De:" . $name . " < ". $from . " > " . "\n\n" . " Escribio lo siguiente:" . "\n\n" . $_POST['message'];
+    $message2 = "Hola " . $name . "\n\n" . "Esta es una copia de tu mensaje " . "\n\n" . $_POST['message'];
 
-    $headers = "De:" . $from;
-    $headers2 = "De:" . $to;
+    $headers = "";
+    $headers2 = "";
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mensaje enviado. Gracias " . $first_name . ", nos contactaremos a la brevedad.";
+    // echo "Mensaje enviado. Gracias " . $first_name . ", nos contactaremos a la brevedad.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
-    }
+    header('Location: index.html');
+}
 ?>
