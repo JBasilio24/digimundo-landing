2680a710ef51e0ea997ff4396e9821704094bcd8
Itzli <itzli2000@msn.com>
2017-10-02 13:22:07 -0500

    Moddified js smooth scroll to avoid final jump

--- statics/js/main.js
+++ statics/js/main.js
@@ -49,8 +49,8 @@ if (
   ) {
 // Figure out element to scroll to
 var target = $(this.hash);
-var navHeight = $('#navbar-main').height();
-var scrollToPosition = target.offset().top - (navHeight);
+var navHeight = $('#navbar-container').height();
+var scrollToPosition = target.offset().top - (navHeight*1.5);
 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 // Does a scroll target exist?
 if (target.length) {
@@ -58,18 +58,7 @@ if (target.length) {
 event.preventDefault();
 $('html, body').animate({
   scrollTop: scrollToPosition
-}, 1500, function() {
-// Callback after animation
-// Must change focus!
-var $target = $(target);
-$target.focus();
-if ($target.is(":focus")) { // Checking if the target was focused
-  return false;
-} else {
-$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-$target.focus(); // Set focus again
-};
-});
+}, 1500, function() {});
 }
 }
 });
