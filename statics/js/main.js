2022de6433a6d344edf7c07c7ea2f25bcab0e896
root <charlixd.cm@gmail.com>
2018-02-10 02:36:33 -0600

    Fixed banner "Nuestros Clientes" alrady is infinity, changed size to whatsapp button. Fixed responsive design in template "asesoria" and template "index".

--- statics/js/main.js
+++ statics/js/main.js
@@ -82,7 +82,6 @@ nextinput++;
 campo = '<li id="rut'+nextinput+'">Campo:<input type="text" size="20" id="campo' + nextinput + '"&nbsp; name="campo' + nextinput + '"&nbsp; /></li>';
 $("#campos").append(campo);
 }
-
 //other
 $(document).ready(function(){
 	var max_phone = 3;
